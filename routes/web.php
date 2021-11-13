ac58eac924b83ae3b6569a7adad032c5b882f6d3
sireax <sireax@yandex.ru>
2021-11-13 04:00:56 +0400

    nginx config fixed, removed index view

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    abort(404);
 });
 
-Route::get('/auth0/callback', [Auth0Controller::class, 'callback'])->name('auth0-callback');
